4d10b1df4b427b35c9c6517b7e6cdc4279aa82ab
KenForever1 <2962666298@qq.com>
2025-07-20 14:28:45 +0800

    add related post

--- overrides/hooks/related_posts.py
+++ overrides/hooks/related_posts.py
@@ -368,6 +368,7 @@ def on_page_markdown(markdown, **kwargs):
         relative_url = article_info['url']
         # 拼接基本路径和文章相对URL，并确保路径分隔符正确
         full_url = (base_path + relative_url).replace('//', '/')
+        full_url = full_url.replace('/KenForever1', '')
         recommendation_html += f'<li><a href="{full_url}">{title}</a></li>\n'
 
     recommendation_html += '</ul>\n'
